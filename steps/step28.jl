fc1dfb583fb0cc025ac338e1f579d90ad22930f6
k-fog <59814110+k-fog@users.noreply.github.com>
2021-05-02 20:41:44 +0900

    feat: add step28

---
+++ steps/step28.jl
@@ -0,0 +1,25 @@
+# step28
+
+using DeZero
+using Plots
+
+rosenbrock(x1,x2) = 100 * (x2 - x1^2)^2 + (x1 - 1)^2
+
+function main()
+    x1 = Variable([0.0])
+    x2 = Variable([2.0])
+    lr = 1e-3
+    iters = 10000
+
+    for i in 1:iters
+        println(x1.data, x2.data)
+        y = rosenbrock(x1, x2)
+        cleargrad(x1)
+        cleargrad(x2)
+        backward!(y)
+        x1.data -= lr * x1.grad
+        x2.data -= lr * x2.grad
+    end
+end
+
+@time main()
